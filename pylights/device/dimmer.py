bf09e4fae4973c53cdef913ddf46dc5bffc77142
Staffan Thomén <duck@shangtai.net>
2017-07-14 08:37:09 +0300

    Don't set Dimmer widgets if we're currently moving the slider

--- pylights/device/dimmer.py
+++ pylights/device/dimmer.py
@@ -10,19 +10,27 @@ class Dimmer(Device):
 		self.label=Label(self, text=title)
 		self.label.pack()
 		self.scale=Scale(self, from_=0, to=255, orient=HORIZONTAL)
-		self.scale.bind('<ButtonRelease-1>', self.press)
+		self.scale.bind('<ButtonPress-1>', self.press)
+		self.scale.bind('<ButtonRelease-1>', self.release)
 		self.scale.pack()
+
+		self.moving=False
 
 	def set(self, value):
 		super().set(value)
 		self.scale.set(value)
 
 	def get(self):
-		if self.getcallback:
+		if not self.moving and self.getcallback:
 			self.set(self.getcallback(*self.getparams))
 
 		return super().get()
 
 	def press(self, event):
+		self.moving=True
+
+	def release(self, event):
+		self.moving=False
+
 		if self.setcallback:
 			self.setcallback(self.scale.get(), *self.setparams)
